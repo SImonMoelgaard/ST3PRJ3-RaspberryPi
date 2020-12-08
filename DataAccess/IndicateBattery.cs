04842f400bc1e9a3e1bf30b4ac1fa1d2273909e0
Annesofie Nørager <au582327@uni.au.dk>
2020-12-08 09:47:07 +0100

    Tænd en Led

--- DataAccess/IndicateBattery.cs
+++ DataAccess/IndicateBattery.cs
@@ -16,20 +16,28 @@ namespace DataAccessLogic
 
         //SoftPmw softpmw= new SoftPmw(); 
         //GPIO ledGpio= new GPIO();
+        private static GpioController _gpioController;
+
+        private const int _batteryLed = 4;
         /// <summary>
         /// atribut, der definere LED'en, der skal indikere Batteristatus 
         /// </summary>
        // private LED BatteryLED;
-        /// <summary>
-        ///Får LED'en til at blinke hvis batteristatusen er under xx% 
-        /// </summary>
+       /// <summary>
+       ///Får LED'en til at blinke hvis batteristatusen er under xx% 
+       /// </summary>
+       public IndicateBattery()
+       {
+           _gpioController = new GpioController();
+           _gpioController.OpenPin(_batteryLed, PinMode.Output);
+        }
         
         public void IndicateLowBattery()
         {
            //var led= softpmw.Create(18,0,100);
             //ledGpio.PinMode(18, 1);
           
-
+         _gpioController.Write(_batteryLed,PinValue.High);
         }
     }
 }
